32be14219b63986ec5b0b4184c04993862f609c3
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-10-30 12:09:54 -0700

    * workflow

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     test = str()
+    l = list()
     return render(request, 'index.html')
 
 PASSWORD = "mysecretpassword123" 
