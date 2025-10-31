1e4ff6d3e7a045fd1db79746c017abdbaf9837c2
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-10-31 15:41:14 -0700

    * workflow

--- app/views.py
+++ app/views.py
@@ -4,12 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-PASSWORD = "mytestpassword123" 
-
-def authenticate(username, password):
-    test = str()
-    l = list()
-    d = dict()
-    if username == "admin" and password == PASSWORD:
-        return True
-    return False
+# PASSWORD = "mytestpassword123" 
+# def authenticate(username, password):
+#     test = str()
+#     l = list()
+#     d = dict()
+#     if username == "admin" and password == PASSWORD:
+#         return True
+#     return False
