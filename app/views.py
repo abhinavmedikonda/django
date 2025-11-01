273433240c7b9be0fb088e498a4ab69cac0c875e
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-11-01 09:35:56 -0700

    * workflow

--- app/views.py
+++ app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+PASSWORD = "mysecretpassword123" 
+def authenticate(username, password):
+    test = str()
+    l = list()
+    if username == "admin" and password == PASSWORD:
+        return True
+    return False
